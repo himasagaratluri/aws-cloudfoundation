787bb72f316c14bff7f05e46d0a3e7ed775e7954
himasagaratluri <himasagar.atluri@gmail.com>
2020-09-15 21:46:42 -0500

    Removed KMs: tagging report doesn't work with KMS

--- infra/organizations/tagging-policy/kms.tf
+++ infra/organizations/tagging-policy/kms.tf
@@ -1,11 +1,11 @@
-resource "aws_kms_key" "kms_key" {
-  description             = "This key is used to encrypt the reports bucket"
-  deletion_window_in_days = 10
+#resource "aws_kms_key" "kms_key" {
+#  description             = "This key is used to encrypt the reports bucket"
+#  deletion_window_in_days = 10
+#
+#  policy = file("${path.module}/policies/kms_policy.json")
+#}
 
-  policy = file("${path.module}/policies/kms_policy.json")
-}
-
-resource "aws_kms_alias" "kms_key_alias" {
-  name          = "alias/haops/tagging-reports-kms"
-  target_key_id = aws_kms_key.kms_key.key_id
-}
+#resource "aws_kms_alias" "kms_key_alias" {
+#  name          = "alias/haops/tagging-reports-kms"
+#  target_key_id = aws_kms_key.kms_key.key_id
+#}
