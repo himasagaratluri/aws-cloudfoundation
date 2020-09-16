e5e95bd082c10e63517f819e969d87ce3983c89c
himasagaratluri <himasagar.atluri@gmail.com>
2020-09-15 21:48:13 -0500

    More parameters

--- infra/organizations/tagging-policy/variables.tf
+++ infra/organizations/tagging-policy/variables.tf
@@ -1,3 +1,15 @@
 variable "target_account_id" {
     default = "719752468914"
 }
+
+variable "bucket_name" {
+    default = "aws-haops-tagging-reports"
+}
+
+variable "org_id" {
+    default = "o-dfplqpbs08"
+}
+
+variable "policy_name" {
+    default = "aws-haops-tags-policy"
+}
