766bd8b8d022a6df6161d6db17ceac52ec47e365
himasagar atluri <himasagar.atluri@gmail.com>
2021-06-20 20:56:49 -0500

    saving stage

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "halturi-terrafrom-state-backend"
+    key    = "codebuild/test"
+    region = "us-east-1"
+  }
+}
+
 module "test_repo" {
   source = "./infra/ci-cd/aws"
 }
